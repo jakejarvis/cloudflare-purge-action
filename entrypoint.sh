2025f6c388ad6920f13ac695a83b69defd20d26b
Jake Jarvis <jake@jarv.is>
2019-09-12 12:58:19 -0400

    Check for required environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+if [ -z "$CLOUDFLARE_ZONE" ]; then
+  echo "CLOUDFLARE_ZONE is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_EMAIL" ]; then
+  echo "CLOUDFLARE_EMAIL is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_KEY" ]; then
+  echo "CLOUDFLARE_KEY is not set. Quitting."
+  exit 1
+fi
+
 curl -sS -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
      -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
      -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
