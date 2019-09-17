f12dbef250f20befce1459b8901cf1419260441b
Jake Jarvis <jake@jarv.is>
2019-09-16 21:28:31 -0400

    Fail pipeline and print errors if API doesn't return an OK status

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,25 @@ if [ -z "$CLOUDFLARE_KEY" ]; then
   exit 1
 fi
 
-curl -sS -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
-     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-     -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"purge_everything":true}'
+# Call the API and store the response for later.
+HTTP_RESPONSE=$(curl -sS -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/purge_cache" \
+         -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+         -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
+         -H "Content-Type: application/json" \
+         --data ${DATA_ARG} \
+         --write-out "HTTP_STATUS:%{http_code}")
+
+# Store result and HTTP status code separately to appropriately throw CI errors.
+# https://gist.github.com/maxcnunes/9f77afdc32df354883df
+HTTP_BODY=$(echo $HTTP_RESPONSE | sed -E 's/HTTP_STATUS\:[0-9]{3}$//')
+HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -E 's/.*HTTP_STATUS:([0-9]{3})$/\1/')
+
+# Fail pipeline and print errors if API doesn't return an OK status.
+if [ $HTTP_STATUS -eq "200" ]; then
+  echo "Successfully purged!"
+  exit 0
+else
+  echo "Purge failed. API response was:"
+  echo $HTTP_BODY
+  exit 1
+fi
